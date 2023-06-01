446a23a160e122384026e7d23dd147558f0ce87a
snouvanda <snouvanda@gmail.com>
2023-06-01 16:31:53 +0700

    create Procurement route and controller

---
+++ src/controllers/transactions/procurementController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express"
+
+export const createProcurement = async (req: Request, res: Response) => {
+  console.log("")
+  console.log("User try to create a new procurement.")
+  return {}
+}
